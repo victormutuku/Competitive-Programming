f87b5289da599c42a62051c6dbe95d6a20acc950
Victor Mutuku <victor.mutuku@strathmore.edu>
2022-11-14 15:01:47 +0300

    FizzBuzz

---
+++ src/Week 1/FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<String>();
+        for(int i = 1; i < n+1; i++){
+
+            if(i%3 == 0 && i%5 == 0){
+                output.add("FizzBuzz");
+            }
+            if(i%3 != 0 && i%5 == 0){
+                output.add("Buzz");
+            }
+            if(i%3 == 0 && i%5 != 0){
+                output.add("Fizz");
+            }
+            if(i%3 != 0 && i%5 != 0){
+                output.add(String.valueOf(i));
+            }
+        }
+        return output;
+    }
+}
