8dd38324b5ce39732ee566c07d2d624d3be68f33
Victor Mutuku <victor.mutuku@strathmore.edu>
2022-11-14 18:34:39 +0300

    Domino Piling

---
+++ src/Week 1/DominoPiling.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m,n;
+        m = scanner.nextInt();
+        n = scanner.nextInt();
+        scanner.close();
+
+        int ans = (m * n)/2;
+        System.out.println(ans);
+
+
+        
+    }
+
+}
