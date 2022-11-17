f4ac09cfc65ac43cf25753ccbed327a5636520c9
Victor Mutuku <victor.mutuku@strathmore.edu>
2022-11-17 11:36:29 +0300

    Renamed folder

---
+++ src/Week1/ValidateSubsequence.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Result {
+    public boolean validate(List<Integer> arr, List<Integer> sequence) {
+        boolean ans = false;
+        int count = 0;
+        List<Integer> newarray = new ArrayList<Integer>();
+        // remove duplicate values
+        for(int k = 0; k < arr.size(); k++){
+            if(!newarray.contains(arr.get(k))){
+            newarray.add(arr.get(k));
+            }
+        }
+
+        for(int i = 0; i < sequence.size(); i++){
+            for(int j = 0; j < newarray.size(); j++) {
+                if(sequence.get(i)==newarray.get(j)){
+                    count++;
+                }
+            }
+        }
+
+        if(count == sequence.size()){
+            ans = true;
+        }
+
+        return ans;
+    }
+
+}
+
+public class ValidateSubsequence {
+    public static void main(String[] args) {
+        List<Integer>arr = new ArrayList<Integer>();
+        arr.add(5);
+        arr.add(1);
+        arr.add(22);
+        arr.add(25);
+        arr.add(6);
+        arr.add(-1);
+        arr.add(8);
+        arr.add(10);
+        
+        List<Integer> seq = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(6);
+        arr.add(-1);
+        arr.add(5);
+        
+        Result rs = new Result();
+        System.out.println(rs.validate(arr, seq)); 
+        
+    }
+}
