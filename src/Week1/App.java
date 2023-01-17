47349f9e8c59a82de4fd2b7ce693046b7cd93ef5
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-01-17 16:13:16 +0300

    Commented/Removed unnecessary code

--- src/Week1/App.java
+++ src/Week1/App.java
@@ -64,7 +64,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        // int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Integer::parseInt)
